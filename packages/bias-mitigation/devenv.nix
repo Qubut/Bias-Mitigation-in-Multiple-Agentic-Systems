007e759f856b24d843998e2ac0225b76fea6dfb3
Qubut <s-aahmed@haw-landshut.de>
2025-12-15 08:58:37 +0100

    chore(devenv): define project's devenv

---
+++ packages/bias-mitigation/devenv.nix
@@ -0,0 +1,43 @@
+{pkgs, ...}:
+{
+  imports = [./modules/python.nix];
+
+  devcontainer.enable = true;
+
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.glib
+      pkgs.zlib
+      pkgs.libglvnd
+      pkgs.xorg.libX11
+      pkgs.openssl
+      pkgs.zeromq
+    ];
+  };
+
+  packages = with pkgs; [
+    gcc
+    libgcc
+    gnumake
+    cmake
+    extra-cmake-modules
+    uv
+    ruff
+    black
+    isort
+    zip
+    ninja
+    zeromq
+    litellm
+  ];
+
+  git-hooks.hooks = {
+    black.enable = true;
+    ruff.enable = true;
+    mypy.enable = true;
+  };
+
+  dotenv.disableHint = true;
+  cachix.enable = false;
+}
